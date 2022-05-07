6d2fc3229aa51a595a7cdabd006f20a6750fdbfa
Arj-01 <aarjitgiri@gmail.com>
2022-05-07 14:01:18 +0530

    new2

--- App.js
+++ App.js
@@ -1,38 +1,32 @@
-import React from 'react'
-import { Provider } from 'react-native-paper'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import { theme } from './src/core/theme'
-import {
-  StartScreen,
-  LoginScreen,
-  RegisterScreen,
-  ResetPasswordScreen,
-  Dashboard,
-} from './src/screens'
+   import React from 'react';
 
-const Stack = createStackNavigator()
-
-export default function App() {
-  return (
-    <Provider theme={theme}>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="StartScreen"
-          screenOptions={{
-            headerShown: false,
-          }}
-        >
-          <Stack.Screen name="StartScreen" component={StartScreen} />
-          <Stack.Screen name="LoginScreen" component={LoginScreen} />
-          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-          <Stack.Screen name="Dashboard" component={Dashboard} />
-          <Stack.Screen
-            name="ResetPasswordScreen"
-            component={ResetPasswordScreen}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </Provider>
-  )
-}
+   import Login from './screens/Login';
+   import Signup from './screens/Signup';
+   
+   import { NavigationContainer } from '@react-navigation/native';
+   import { createStackNavigator } from '@react-navigation/stack';
+   
+   
+   const Stack = createStackNavigator();
+   
+   
+   
+   function App() {
+     return (
+       <Stack.Navigator screenOptions={{headerShown: false}} >
+         <Stack.Screen name="Login" component={Login} />
+         <Stack.Screen name="Signup" component={Signup} />
+       </Stack.Navigator>
+     );
+   }
+   
+   
+   export default () => {
+     return (
+       <NavigationContainer>
+        
+           <App />
+         
+       </NavigationContainer>
+     )
+   }
